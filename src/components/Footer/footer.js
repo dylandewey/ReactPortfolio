93827857f55ba7147cc643f49f9a894b821a817c
dylandewey <dylandewey41@gmail.com>
2020-10-19 16:29:02 -0600

    figuring out the file structure

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,38 @@
-import React from "react";
-import "./style.css";
+import React, { Component } from 'react';
 
-function Footer() {
+class Footer extends Component {
+  render() {
+
+    if(this.props.data){
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
     return (
-        <footer className="footer">
-            <span>Pupster 2017</span>
-        </footer>
+      <footer>
+
+     <div className="row">
+        <div className="twelve columns">
+           <ul className="social-links">
+              {networks}
+           </ul>
+
+           <ul>
+            <li>Footer</li>
+           </ul>
+
+           <ul className="copyright">
+              <li>&copy; 2020</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+           </ul>
+
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+  </footer>
     );
+  }
 }
 
 export default Footer;
