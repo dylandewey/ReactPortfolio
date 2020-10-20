d224dcdc8acb7aaca4838cf11d57c3d5151c4f78
dylandewey <dylandewey41@gmail.com>
2020-10-19 18:45:46 -0600

    changing homepage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
+// import Container from "../components/Container";
+// import Row from "../components/Row";
+// import Col from "../components/Col";
 
 function About() {
   return (
     <div>
-      <Hero backgroundImage="https://i.imgur.com/qkdpN.jpg">
-        <h1>Pupster</h1>
-        <h2>They're the Good Boys and Girls</h2>
+      <Hero backgroundImage="./monster.JPG">
+        <h1>Dylan Dewey</h1>
+        <h2>Full Stack Web Developer</h2>
       </Hero>
-      <Container style={{ marginTop: 30 }}>
+      {/* <Container style={{ marginTop: 30 }}>
         <Row>
           <Col size="md-12">
-            <h1>Welcome To Pupster!</h1>
+            <h1></h1>
           </Col>
         </Row>
         <Row>
@@ -56,7 +56,7 @@ function About() {
             </p>
           </Col>
         </Row>
-      </Container>
+      </Container> */}
     </div>
   );
 }
